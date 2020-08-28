7ada8c09c5903f99ff0fa1f3156908c0896c7f91
karleighponnock <karleighponnock@gmail.com>
2020-08-28 16:13:07 -0400

    Updates

---
+++ precache-manifest.d2b544b5df8ae5c699d501f946be9ae1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53585b3de4016bd9719f789233cfc77f",
+    "url": "/karleighponnock/todo_list_2/index.html"
+  },
+  {
+    "revision": "166e6fbbe44ae90dab88",
+    "url": "/karleighponnock/todo_list_2/static/css/main.ddfa09ed.chunk.css"
+  },
+  {
+    "revision": "15147f3312a34dab7d0f",
+    "url": "/karleighponnock/todo_list_2/static/js/2.832ea22d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/karleighponnock/todo_list_2/static/js/2.832ea22d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "166e6fbbe44ae90dab88",
+    "url": "/karleighponnock/todo_list_2/static/js/main.58fb2415.chunk.js"
+  },
+  {
+    "revision": "2e48a4d0be4aaf95ed0f",
+    "url": "/karleighponnock/todo_list_2/static/js/runtime-main.73a2c530.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/karleighponnock/todo_list_2/static/media/logo.5d5d9eef.svg"
+  }
+]);

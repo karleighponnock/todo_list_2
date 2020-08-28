c711b98cf81f3084c5ae199d042442feb2d9da1a
karleighponnock <karleighponnock@gmail.com>
2020-08-28 15:24:54 -0400

    set the update after delete item: items are successfully being added and deleted

--- todolist2/src/App.js
+++ todolist2/src/App.js
@@ -5,7 +5,6 @@ import ListItems from './ListItems'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-
 library.add(faTrash);
 
 class App extends React.Component {
@@ -33,41 +32,44 @@ class App extends React.Component {
     }
     addItem(e){
       e.preventDefault();
-      const newItem = this.state.currentItem
-      if(newItem.text!==""){
-        const newItems=[...this.state.items, newItem];
-        this.setState({
-          items:newItems,
-          currentItem:{
-            text:'',
-            key:''
-          }
-        })
+      const newItem = this.state.currentItem;
+      if(newItem.text !==""){
+        const items = [...this.state.items, newItem];
+      this.setState({
+        items: items,
+        currentItem:{
+          text:'',
+          key:''
+        }
+      })
       }
     }
-    deleteItem(key){
-      const filteredItems= this.state.items.filter(item =>
-        item.key!==key);
-      this.setState({
-        items: filteredItems
-      })
-    }
+      deleteItem(key){
+        const filteredItems= this.state.items.filter(item =>
+          item.key!==key);
+        this.setState({
+          items: filteredItems
+        })
+    
+      }
 
-  render() {
-    return (
-      <div className="App">
-        <header>
-        <form id="to-do-form" onSubmit={this.addItem}>
-          <input type="text" placeholder="Enter task" value= {this.state.currentItem.text} onChange={this.handleInput}></input>
-          <button type="submit">Add</button>
-        </form>
-        </header>
-        <ListItems items={this.state.items}>
-          deleteItem = {this.deleteItem}>
-        </ListItems>
-      </div>
-    )
-  }
-}
-
-export default App;
+      render(){
+        return (
+          <div className="App">
+            <header>
+              <form id="to-do-form" onSubmit={this.addItem}>
+                <input type="text" placeholder="Enter task" value= {this.state.currentItem.text} onChange={this.handleInput}></input>
+                <button type="submit">Add</button>
+              </form>
+              <p>{this.state.items.text}</p>
+              
+                <ListItems items={this.state.items} deleteItem={this.deleteItem} setUpdate={this.setUpdate}/>
+              
+            </header>
+          </div>
+        );
+       }
+      }
+      
+      
+      export default App;
--- todolist2/src/ListItems.js
+++ todolist2/src/ListItems.js
@@ -1,25 +1,30 @@
-import React from "react";
-import "./ListItems.css";
+import React from 'react';
+import './ListItems.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 function ListItems(props){
     const items = props.items;
-    const ListItems = items.map(item =>
-        {
-        return <div className="list" key="item.key">
-            <p>{item.text}
-            <span>
-                <FontAwesomeIcon className="faicons" 
-                icon="trash"
-                onClick={ () => props.deleteItem(item.key)
-                }/>
-            </span>
-            </p> 
-        </div>
-    })
-    return(
-        <div>{ListItems}</div>
-    )
-}
+    const listItems = items.map(item =>
+   {
+       return <div className="list" key={item.key}>
+     <p>
+         <input type="text" id={item.key} value={item.text} onChange={(e)=>{
+             props.setUpdate(e.target.value,item.key)}}/>
+        <span>
+       
+        <FontAwesomeIcon className="faicons" onClick={() => {
+            props.deleteItem(item.key)
+        }} icon="trash" />
+        </span>
+     </p>
+     
+    </div>})
+    return <div>
+        
+        {listItems}
+       
+    </div>;
+  }
 
-export default ListItems;
+  export default ListItems;

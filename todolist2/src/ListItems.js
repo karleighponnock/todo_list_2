b5322337002b8873468cf83c495596e7f1f02021
karleighponnock <karleighponnock@gmail.com>
2020-08-28 15:46:13 -0400

    fix syntax error. application now can add, delete, edit items and are animated on add/delete

--- todolist2/src/ListItems.js
+++ todolist2/src/ListItems.js
@@ -7,24 +7,25 @@ function ListItems(props){
     const items = props.items;
     const listItems = items.map(item =>
    {
-       return <div className="list" key={item.key}>
-     <p>
-         <input type="text" id={item.key} value={item.text} onChange={(e)=>{
-             props.setUpdate(e.target.value,item.key)}}/>
-        <span>
-       
-        <FontAwesomeIcon className="faicons" onClick={() => {
-            props.deleteItem(item.key)
-        }} icon="trash" />
-        </span>
-     </p>
-     
-    </div>})
-    return <div>
-       <FlipMove duration={500} easing="easing-in-out">
-         {listItems}
+    return <div className="list" key={item.key}>
+    <p>
+        <input type="text" id={item.key} value={item.text} onChange={(e)=>{
+            props.setUpdate(e.target.value,item.key)}}/>
+       <span>
+      
+       <FontAwesomeIcon className="faicons" onClick={() => {
+           props.deleteItem(item.key)
+       }} icon="trash" />
+       </span>
+    </p>
+    
+   </div>})
+   return <div>
+       <FlipMove duration={300} easing="ease-in-out">
+       {listItems}
        </FlipMove>
-    </div>;
-  }
+   
+   </div>;
+ }
 
   export default ListItems;

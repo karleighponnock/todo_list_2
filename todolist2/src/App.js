0787c36894b076f837208c84df4194980c56fcf2
karleighponnock <karleighponnock@gmail.com>
2020-08-28 13:27:36 -0400

    style existing code

--- todolist2/src/App.js
+++ todolist2/src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component{
-  render(){
-    return(
+class App extends React.Component {
+  render() {
+    return (
       <div className="App">
-      <header>
-        <form id="to-do-form">
-          <input type="text" placeholder="Enter Text"/>
-          <button type="submit">Add</button>
-        </form>
-      </header>
+        <header>
+          <form id="to-do-form">
+            <input type="text" placeholder="Enter Text" />
+            <button type="submit">Add</button>
+          </form>
+        </header>
       </div>
     )
   }

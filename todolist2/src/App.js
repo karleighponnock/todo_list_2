b52d304fe19c491cb1f8fcabeffcef164e39bc16
karleighponnock <karleighponnock@gmail.com>
2020-08-28 14:26:16 -0400

    fixed syntax errors

--- todolist2/src/App.js
+++ todolist2/src/App.js
@@ -3,27 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
-    constructor(props){
-      super(props);
-      this.state-{
-        items:[],
-        currentItem:{
-          text:'',
-          key:''
-        }
+  constructor(props){
+    super(props);
+    this.state = {
+      items:[],
+      currentItem:{
+        text:'',
+        key:''
       }
     }
-
+    }
   render() {
     return (
       <div className="App">
         <header>
-          <form id="to-do-form" onSubmit={this.addItem}>
-            <input type="text" placeholder="Enter Text"
-            value={this.currentItem.text}
-            onChange={this.handleInput}/>
-            <button type="submit">Add</button>
-          </form>
+        <form id="to-do-form" onSubmit={this.addItem}>
+          <input type="text" placeholder="Enter task" value= {this.state.currentItem.text} onChange={this.handleInput}></input>
+          <button type="submit">Add</button>
+        </form>
         </header>
       </div>
     )

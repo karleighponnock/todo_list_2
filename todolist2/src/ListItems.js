bc0dfb47a5656d3b7a63fbec30bc554815d07180
karleighponnock <karleighponnock@gmail.com>
2020-08-28 14:55:45 -0400

    create a variable to map over the list items then return them in a div for the componenet

--- todolist2/src/ListItems.js
+++ todolist2/src/ListItems.js
@@ -3,8 +3,14 @@ import "./ListItems.css";
 
 function ListItems(props){
     const items = props.items;
+    const ListItems = items.map(item =>
+        {
+        return <div className="list" key="item.key">
+            <p>{item.text}</p>
+        </div>
+    })
     return(
-
+        <div>{ListItems}</div>
     )
 }
 
